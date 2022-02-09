46248af0bba4b2d63f534d7d8963c77c9f51a164
Alicia Serrano Martinez <aserranomar@unav.es>
2022-02-09 11:33:51 +0100

    Sub function added

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,25 +1,32 @@
 # -*- coding: utf-8 -*-
 from calculator.main import Calc 
 
-def test_add_two_numbers():
-    calculator = Calc() #Instantation
+    def test_add_two_numbers():
+        calculator = Calc() #Instantation
     
-    result = calculator.add(2, 3)
+        result = calculator.add(2, 3)
     
-    assert result == 5
+        assert result == 5
     
     
-def test_add_three_numbers():
-    calculator = Calc() #Instantation
+    def test_add_three_numbers():
+        calculator = Calc() #Instantation
+       
+        result = calculator.add(2, 3, 4)
     
-    result = calculator.add(2, 3, 4)
+        assert result == 9
     
-    assert result == 9
+    def test_add_many_numbers():
+        numbers = range(1,100)
+        calculator = Calc() #Instantation
     
-def test_add_many_numbers():
-    numbers = range(1,100)
-    calculator = Calc() #Instantation
+        result = calculator.add(*numbers)
     
-    result = calculator.add(*numbers)
+        assert result == 4950
     
-    assert result == 4950
+    def test_subtract_two_numbers():
+        calculator = Calc() #Instantation
+    
+        result = calculator.sub(10, 4)
+    
+        assert result == 6
