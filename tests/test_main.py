a27b28ba9660f6dde73a85554dff4acabe9ced84
Alicia Serrano Martinez <aserranomar@unav.es>
2022-02-09 11:31:57 +0100

    Add function added

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,2 +1,25 @@
 # -*- coding: utf-8 -*-
+from calculator.main import Calc 
 
+def test_add_two_numbers():
+    calculator = Calc() #Instantation
+    
+    result = calculator.add(2, 3)
+    
+    assert result == 5
+    
+    
+def test_add_three_numbers():
+    calculator = Calc() #Instantation
+    
+    result = calculator.add(2, 3, 4)
+    
+    assert result == 9
+    
+def test_add_many_numbers():
+    numbers = range(1,100)
+    calculator = Calc() #Instantation
+    
+    result = calculator.add(*numbers)
+    
+    assert result == 4950
